c9246ddd9d967d8a032d21ee2314f774d717f0a3
saurabhm0709 <saurabhmishra0619@gmail.com>
2024-06-25 20:52:03 +0530

    Add files via upload
    This is a simple command-line guessing game where the player has to guess a secret number between 1 and 20. The game provides feedback on whether the guess is too high or too low and keeps track of the player's highest score. Each player starts with 5 attempts to guess the correct number, and the game offers the option to play again after each round

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+
+highest_score = 0
+
+def play_again():
+  again = input('Want to play again ?Enter Y or N: ').lower()
+  print()
+  return again == 'y'
+
+def guess_number():
+
+  current_score = 5
+  secret_number = random.randint(1,20)
+
+  global highest_score
+
+  while current_score > 0:       
+    
+    guess = int(input("Enter a number between 1 and 20: "))
+    print()
+    if guess == secret_number:
+      print("Congrats! your guess is correct.")
+      print()
+      if current_score > highest_score:
+        highest_score = current_score
+      print('score: ',current_score)
+      print('Highest score: ',highest_score)
+      print()
+      if play_again():
+        current_score = 5
+        secret_number = random.randint(1,20)
+        continue
+      else:
+        break
+    elif guess!= secret_number and guess > secret_number:
+      print("Too high!")
+      current_score -= 1
+      print('Current Score: ',current_score)
+      print()
+    elif guess != secret_number and guess < secret_number:
+      print("Too low!")
+      current_score -= 1
+      print('Current Score: ',current_score)
+      print()
+  if current_score <= 0:
+    print('You lost all the chances!')
+    if play_again():
+      guess_number()
+    print()
+
+guess_number()
